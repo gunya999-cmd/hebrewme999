a3deec3868a91089e71bc1ce87238fbe5a82aefa
gunya999-cmd <gunya999@gmail.com>
2026-07-22 17:43:09 +0300

    Add responsive Verb Drops wrapper

---
+++ src/pages/VerbDropsResponsive.tsx
@@ -0,0 +1,10 @@
+import VerbDropsGame from "@/pages/VerbDropsGame";
+import "@/styles/verb-drops-tablet.css";
+
+export default function VerbDropsResponsive() {
+  return (
+    <div className="verb-drops-tablet">
+      <VerbDropsGame />
+    </div>
+  );
+}
